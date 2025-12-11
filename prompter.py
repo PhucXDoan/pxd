ce395a8b400098321b99027c70c0043cc8f2eacf
Phuc X. Doan <phucxdoan@gmail.com>
2025-12-10 23:44:22 -0500

    Fleshing out `prompter.py` more.

--- prompter.py
+++ prompter.py
@@ -1,4 +1,49 @@
-import types
+import types, difflib, sys
+
+
+
+RESET       = '\x1B[0m'
+BOLD        = '\x1B[1m'
+UNDERLINE   = '\x1B[4m'
+FG_BLACK    = '\x1B[30m'
+FG_RED      = '\x1B[31m'
+FG_GREEN    = '\x1B[32m'
+FG_YELLOW   = '\x1B[33m'
+FG_BLUE     = '\x1B[34m'
+FG_MAGENTA  = '\x1B[35m'
+FG_CYAN     = '\x1B[36m'
+FG_WHITE    = '\x1B[37m'
+BG_BLACK    = '\x1B[40m'
+BG_RED      = '\x1B[41m'
+BG_GREEN    = '\x1B[42m'
+BG_YELLOW   = '\x1B[43m'
+BG_BLUE     = '\x1B[44m'
+BG_MAGENTA  = '\x1B[45m'
+BG_CYAN     = '\x1B[46m'
+BG_WHITE    = '\x1B[47m'
+
+
+
+def did_you_mean(message, given, options):
+
+    suggestions = difflib.get_close_matches(
+        given,
+        [str(option) for option in options],
+    )
+
+    message += '\n'
+
+    for suggestion_i, suggestion in enumerate(suggestions):
+        line     = '... or {}?' if suggestion_i else 'Did you mean {}?'
+        line     = ' ' * (message.index('{}') - line.index('{}')) + line
+        message += line + '\n'
+
+    message = message.format(
+        repr(given),
+        *[repr(suggestion) for suggestion in suggestions]
+    ).removesuffix('\n')
+
+    return message
 
 
 
@@ -9,11 +54,12 @@ class Interface:
     # Interfaces are where all verbs are
     # grouped together and are eventually invoked.
 
-    def __init__(self, name, logger):
+    def __init__(self, *, name, description, logger):
 
-        self.name   = name
-        self.verbs  = []
-        self.logger = logger
+        self.name        = name
+        self.description = description
+        self.verbs       = []
+        self.logger      = logger
 
 
 
@@ -22,12 +68,141 @@ class Interface:
         @self.new_verb(
             {
                 'name'        : ...,
-                'description' : f'Show detailed usage of {repr(self.name)}.'
+                'description' : f"Show usage of {repr(self.name)}; pass 'all' for all details."
             },
+            {
+                'name'        : 'verb_name',
+                'description' : 'Name of the verb to show more detail on.',
+                'type'        : str,
+                'default'     : None,
+            }
         )
         def help(parameters):
 
-            self.logger.info('meow!')
+            output = ''
+
+
+
+            # Details of the interface itself.
+
+            output += f'> {UNDERLINE}{BOLD}{self.name} [verb] (parameters...){RESET}' '\n'
+            output += f'{self.description}'                                           '\n'
+            output += '\n'
+
+
+
+            # We want to show the `help` last so that
+            # it'll be the first thing the user sees
+            # if the list of verbs is very long.
+
+            shown_verbs = sorted(
+                [
+                    verb
+                    for verb in self.verbs
+                    if parameters.verb_name in (verb.name, None, 'all')
+                ],
+                key = lambda verb: (verb.name == 'help')
+            )
+
+
+
+            # If given a specific verb name as a parameter,
+            # make sure it actually exists.
+
+            if not shown_verbs and parameters.verb_name not in (None, 'all'):
+
+                self.logger.error(did_you_mean(
+                    'No verb goes by the name of {}.',
+                    parameters.verb_name,
+                    [verb.name for verb in self.verbs],
+                ))
+
+                sys.exit(1)
+
+
+
+            # Details of each verb registered in the interface.
+
+            for verb in shown_verbs:
+
+
+
+                # Indicator to show that some verbs were filtered out.
+
+                verbs_were_filtered_out = parameters.verb_name not in (None, 'all')
+
+                if verbs_were_filtered_out:
+                    output += '    ...' '\n'
+                    output += '\n'
+
+
+
+                # Verb name.
+
+                output += f'    > {UNDERLINE}{BOLD}{self.name} {FG_GREEN}{verb.name}{RESET}{UNDERLINE}{BOLD}'
+
+
+
+                # Verb parameters in the invocation.
+
+                for parameter_schema in verb.parameter_schemas:
+
+                    output += f' {parameter_schema.formatted_name}'
+
+                output += f'{RESET}' '\n'
+
+
+
+                # Verb description.
+
+                output += f'    {verb.description}' '\n'
+                output += '\n'
+
+
+
+                # Verb parameter descriptions.
+
+                if parameters.verb_name is not None:
+
+                    for parameter_schema in verb.parameter_schemas:
+
+                        output += f'        {parameter_schema.formatted_name} {parameter_schema.description}' '\n'
+
+
+
+                        # Show that the parameter is optional if applicable.
+
+                        if parameter_schema.has_default:
+
+                            match parameter_schema.default:
+
+                                case str() | int() | float() | bool():
+                                    default = repr(parameter_schema.default)
+
+                                case _: # Not easily representable.
+                                    default = '(optional)'
+
+                            output += f'        = {default}' '\n'
+
+
+
+                        output += '\n'
+
+
+
+                # Indicator to show that some verbs were filtered out.
+
+                if verbs_were_filtered_out:
+                    output += '    ...' '\n'
+                    output += '\n'
+
+
+
+            output = output.removesuffix('\n')
+
+            self.logger.info(output)
+
+        self.help = help
 
 
 
@@ -47,6 +222,16 @@ class Interface:
             if verb_name is ...:
                 verb_name = function.__name__
 
+            if not verb_name.isidentifier():
+                raise ValueError(
+                    f'Verb name {repr(verb_name)} must be an identifier.'
+                )
+
+            if verb_name == 'all':
+                raise ValueError(
+                    f"Verb name {repr(verb_name)} cannot be 'all'."
+                )
+
             if properties_of_verb:
                 raise ValueError(
                     f'Leftover verb properties: {repr(properties_of_verb)}.'
@@ -61,23 +246,63 @@ class Interface:
 
             # Process parameter properties.
 
-            schema_of_parameters = []
+            parameter_schemas = []
 
             for parameter_property in properties_of_parameters:
 
-                parameter_name        = parameter_property.pop('name')
-                parameter_description = parameter_property.pop('description')
-                parameter_type        = parameter_property.pop('type')
+
+
+                # Validate the properties.
+
+                parameter_identifier_name = parameter_property.pop('name')
+                parameter_description     = parameter_property.pop('description')
+                parameter_type            = parameter_property.pop('type')
+                parameter_has_default     = 'default' in parameter_property
+                parameter_default         = parameter_property.pop('default', None)
+                parameter_flag_only       = parameter_property.pop('flag_only', None)
+
+                if not parameter_identifier_name.isidentifier():
+                    raise ValueError(
+                        f'Parameter name {repr(parameter_identifier_name)} must be an identifier.'
+                    )
 
                 if parameter_property:
                     raise ValueError(
                         f'Leftover parameter properties: {repr(parameter_property)}.'
                     )
 
-                schema_of_parameters += [types.SimpleNamespace(
-                    name        = parameter_name,
-                    description = parameter_description,
-                    type        = parameter_type,
+
+
+                # Boolean parameters will default to being flag-only.
+
+                if parameter_type == bool and parameter_flag_only is None:
+                    parameter_flag_only = True
+
+
+
+                # Determine the formatted name.
+
+                parameter_formatted_name = f'{parameter_identifier_name.replace('_', '-')}'
+
+                if parameter_flag_only:
+                    parameter_formatted_name = f'--{parameter_formatted_name}'
+
+                if not parameter_has_default:
+                    parameter_formatted_name = f'*{parameter_formatted_name}'
+
+                parameter_formatted_name = f'({parameter_formatted_name})'
+
+
+
+                # The verb now has a new parameter.
+
+                parameter_schemas += [types.SimpleNamespace(
+                    identifier_name = parameter_identifier_name,
+                    formatted_name  = parameter_formatted_name,
+                    description     = parameter_description,
+                    type            = parameter_type,
+                    has_default     = parameter_has_default,
+                    default         = parameter_default,
                 )]
 
 
@@ -85,10 +310,10 @@ class Interface:
             # Register the new verb.
 
             self.verbs += [types.SimpleNamespace(
-                name                 = verb_name,
-                description          = verb_description,
-                schema_of_parameters = schema_of_parameters,
-                function             = function,
+                name              = verb_name,
+                description       = verb_description,
+                parameter_schemas = parameter_schemas,
+                function          = function,
             )]
 
             return function
@@ -104,36 +329,111 @@ class Interface:
 
 
 
-        # TODO.
+        # Just show the help information if given no arguments.
 
         if not given:
-            raise NotImplementedError
+
+            self.help(types.SimpleNamespace(
+                verb_name = None,
+            ))
+
+            return
 
 
 
-        # TODO.
+        # Search for the verb.
 
-        given_verb_name, *given_arguments = given
+        given_verb_name, *remaining_arguments = given
 
         for verb in self.verbs:
             if verb.name == given_verb_name:
                 break
+
         else:
-            raise NotImplementedError
+
+            self.help(types.SimpleNamespace(
+                verb_name = None,
+            ))
+
+            self.logger.error(did_you_mean(
+                'No verb goes by the name of {}.',
+                given_verb_name,
+                [verb.name for verb in self.verbs],
+            ))
+
+            sys.exit(1)
+
 
 
         # TODO.
 
-        parameters = None
+        parameters = {}
+
 
 
         # TODO.
 
-        for parameter_schema in verb.schema_of_parameters:
+        remaining_parameter_schemas = verb.parameter_schemas[:]
 
-            for argument_i, argument in enumerate(given_arguments):
-                pass
-
+        while remaining_parameter_schemas:
 
 
-        verb.function(parameters)
+
+            # Find an argument that could be matched up
+            # with the next parameter schema in line.
+
+            for argument_i, argument in enumerate(remaining_arguments):
+                break
+            else:
+                break
+
+
+
+            # TODO.
+
+            parameters[remaining_parameter_schemas[0].identifier_name] = remaining_arguments[argument_i]
+
+            del remaining_parameter_schemas[0]
+            del remaining_arguments[argument_i]
+
+
+
+        # TODO.
+
+        for parameter_schema in remaining_parameter_schemas:
+
+            if parameter_schema.has_default:
+
+                parameters[parameter_schema.identifier_name] = parameter_schema.default
+
+
+
+            # Missing required parameter.
+
+            else:
+
+                self.help(types.SimpleNamespace(
+                    verb_name = verb.name,
+                ))
+
+                self.logger.error(f'Missing parameter {parameter_schema.formatted_name}.')
+
+                sys.exit(1)
+
+
+
+        # There shouldn't be any leftover arguments.
+
+        if remaining_arguments:
+
+            self.help(types.SimpleNamespace(
+                verb_name = verb.name,
+            ))
+
+            self.logger.error(f'Extra argument {repr(remaining_arguments[0])}.')
+
+            sys.exit(1)
+
+
+
+        verb.function(types.SimpleNamespace(**parameters))
--- prompter_testbench.py
+++ prompter_testbench.py
@@ -1,6 +1,6 @@
 #! /usr/bin/env python3
 
-import sys, logging
+import sys, logging, pathlib
 import prompter
 
 
@@ -66,16 +66,96 @@ class RootFormatter(logging.Formatter):
 
         return message
 
-logging.basicConfig(level = logging.DEBUG)
-logging.getLogger().handlers[0].setFormatter(RootFormatter())
+
+
 logger = logging.getLogger(__name__)
+logger.setLevel(logging.DEBUG)
+
+logger_handler = logging.StreamHandler(sys.stdout)
+logger_handler.setFormatter(RootFormatter())
+
+logger.addHandler(logger_handler)
 
 
 
+class InterfaceFormatter(logging.Formatter):
+
+    def format(self, record):
 
 
 
-interface = prompter.Interface('meow', logger)
+        message = super().format(record)
+
+
+
+        # The `table` property allows for a
+        # simple, justified table to be outputted.
+
+        if hasattr(record, 'table'):
+
+            for just_key, just_value in justify([
+                (
+                    ('<' , str(key  )),
+                    (None, str(value)),
+                )
+                for key, value in record.table
+            ]):
+                message += f'\n{just_key} : {just_value}'
+
+
+
+        if record.levelname != 'INFO':
+
+            # Any newlines will be indented so it'll look nice.
+
+            indent = ' ' * len(f'[{record.levelname}] ')
+
+            message = '\n'.join([
+                message.splitlines()[0],
+                *[f'{indent}{line}' for line in message.splitlines()[1:]]
+            ])
+
+
+
+            # Give each log a bit of breathing room.
+
+            message += '\n'
+
+
+
+            # Prepend the log level name and color based on severity.
+
+            coloring = {
+                'DEBUG'    : '\x1B[0;35m',
+                'INFO'     : '\x1B[0;36m',
+                'WARNING'  : '\x1B[0;33m',
+                'ERROR'    : '\x1B[0;31m',
+                'CRITICAL' : '\x1B[1;31m',
+            }[record.levelname]
+
+            reset = '\x1B[0m'
+
+            message = f'{coloring}[{record.levelname}]{reset} {message}'
+
+
+
+        return message
+
+
+
+interface_logger = logging.getLogger('interface')
+interface_logger.setLevel(logging.DEBUG)
+
+interface_logger_handler = logging.StreamHandler(sys.stdout)
+interface_logger_handler.setFormatter(InterfaceFormatter())
+
+interface_logger.addHandler(interface_logger_handler)
+
+interface = prompter.Interface(
+    name        = pathlib.Path(__file__).name,
+    description = 'Command-line interface to do stuff.',
+    logger      = interface_logger,
+)
 
 
 @interface.new_verb(
@@ -99,6 +179,50 @@ def some_verb_here(parameters):
     print(parameters)
 
 
+
+@interface.new_verb(
+    {
+        'name'        : 'subtracter',
+        'description' : 'Subtracts two given numbers.',
+    },
+    {
+        'name'        : 'minuend',
+        'description' : 'The value to be subtracted from.',
+        'type'        : bool,
+        'default'     : False,
+    },
+    {
+        'name'        : 'subtrahend',
+        'description' : 'The value to subtracted by.',
+        'type'        : bool,
+        'flag_only'   : True,
+    },
+)
+def some_verb_here(parameters):
+
+    print(parameters)
+
+
+
+@interface.new_verb(
+    {
+        'name'        : 'subtracting',
+        'description' : 'Subtracts two given numbers.',
+    },
+    {
+        'name'        : 'minuend',
+        'description' : 'The value to be subtracted from.',
+        'type'        : int,
+    },
+    {
+        'name'        : 'subtrahend',
+        'description' : 'The value to subtracted by.',
+        'type'        : int,
+    },
+)
+def some_verb_here(parameters):
+
+    print(parameters)
 
 
 
